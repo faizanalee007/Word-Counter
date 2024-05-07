0e1e9bfb90246f3f15b5a6ad106ca0b4150b67b0
faizanalee007 <160243661+faizanalee007@users.noreply.github.com>
2024-05-07 21:33:33 +0500

    created a simple CLI based Word-Count program Typescript code

---
+++ index.ts
@@ -0,0 +1,18 @@
+#! /usr/bin/env node
+
+import inquirer from 'inquirer'
+
+let sentence = await inquirer.prompt(
+    [
+        {
+            name:'sentence',
+            type:' input',
+            message:'Please Type your sentence: ',
+        }
+    ])
+
+let wordCount=sentence.sentence.trim().split(' ');
+
+console.log(wordCount);
+
+console.log(`Your sentence word count is ${wordCount.length}`)
